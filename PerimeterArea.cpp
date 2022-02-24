fb0b281160a226ca559eb5360b78054a293441d4
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-02-24 22:31:47 +0000

    added cpp file

---
+++ PerimeterArea.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Aidan Lalonde-Novales All rights reserved
+//
+// Created by Aidan Lalonde-Novales
+// Created on 2022-02-24
+// This is a program that calculates the area of
+// a rectangle with a size of 5cm x 3cm.
+
+#include <iostream>
+
+int main() {
+  std::cout << "A rectangle with the dimensions" << std::endl;
+  std::cout << "5cm x 3cm will have the following:" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Perimeter of " << (2 * (5 + 3)) << "cm." << std::endl;
+  std::cout << "Area of " << (5 * 3) << "cm²." << std::endl;
+}
