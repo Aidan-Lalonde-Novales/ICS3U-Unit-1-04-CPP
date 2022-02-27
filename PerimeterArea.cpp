017aa91adc14a2b16a452bbf877891344819a4fc
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-02-27 18:42:37 +0000

    fixed space error

--- PerimeterArea.cpp
+++ PerimeterArea.cpp
@@ -11,6 +11,6 @@ int main() {
   std::cout << "A rectangle with the dimensions" << std::endl;
   std::cout << "5cm x 3cm will have the following:" << std::endl;
   std::cout << std::endl;
-  std::cout << "Perimeter of " << (2 * (5 + 3)) << "cm." << std::endl;
-  std::cout << "Area of " << (5 * 3) << "cm²." << std::endl;
+  std::cout << "Perimeter of " << (2 * (5 + 3)) << " cm." << std::endl;
+  std::cout << "Area of " << (5 * 3) << " cm²." << std::endl;
 }
